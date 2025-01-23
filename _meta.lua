45516c4354ff35114a5139edd196272aabb1ef9e
bri <284789+b-@users.noreply.github.com>
2025-01-22 22:27:17 -0500

    initial commit

--- _meta.lua
+++
@@ -1,6 +0,0 @@
-local _ = require("gettext")
-return {
-    name = "filebrowser",
-    fullname = _("Filebrowser"),
-    description = _([[Run Filebrowser server to upload and download files.]]),
-}
---
+++ steps/2-convert-image.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [[ ${#} -ne 3 ]]; then
+    echo "Usage: <source image> <dest image> <platform>"
+    echo ""
+    echo "Example:"
+    echo "./$(basename "${0}") fedora-coreos-40.20240616.3.0-{qemu,proxmox}.x86_64.qcow2 proxmox"
+    exit 1
+fi
+
+source="${1}"
+dest="${2}"
+platform="${3}"
+
+if [[ ! -f "${source}" ]]; then
+    echo "Source image ${source} does not exists"
+    exit 1
+fi
+
+if [[ -f "${dest}" ]]; then
+    echo "Destination image ${dest} already exists"
+    exit 1
+fi
+
+if [[ -z "$(command -v guestfish)" ]]; then
+    echo "Could not find 'guestfish' command"
+    #exit 1
+    sudo apt update
+    sudo apt install -y guestfish
+fi
+
+cp --reflink=auto "${source}" "${dest}"
+guestfish -a "${dest}" <<EOF
+run
+mount /dev/sda3 /
+download /loader/entries/ostree-1.conf tmp.loader.entries.ostree-1.conf
+<! sed -i "s/ignition.platform.id=qemu/ignition.platform.id=${platform}/" tmp.loader.entries.ostree-1.conf
+upload tmp.loader.entries.ostree-1.conf /loader/entries/ostree-1.conf
+EOF
+
+rm -v ./tmp.loader.entries.ostree-1.conf
+
+echo "Done"
